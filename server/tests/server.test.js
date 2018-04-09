63678abd82a341b2b3db45f68890288b32a0c813
Alex Fadeev <alexfd89@gmail.com>
2018-04-09 16:37:39 +0300

    Test GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -3,10 +3,13 @@ const expect = require('expect');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {ObjectID} = require('mongodb');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -67,6 +70,33 @@ describe('GET /todos', () => {
         .expect((res) => {
             expect(res.body.todos.length).toBe(2);
         })
+        .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('Should return todo doc',(done) => {
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo.text).toBe(todos[0].text);
+        })
+        .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var newId = new ObjectID();
+        request(app)
+        .get(`/todos/${newId}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+        .get(`/todos/123`)
+        .expect(404)
         .end(done);
     });
 });
