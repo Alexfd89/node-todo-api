c2b430939574cd482b9e14ee35b566edfc54d741
Alex Fadeev <alexfd89@gmail.com>
2018-04-09 15:35:11 +0300

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose'); 
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
+var {ObjectID} = require('mongodb');
+
 
 var app = express();
 
@@ -26,6 +28,24 @@ app.get('/todos', (req,res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req,res) => {
+
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo})
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
