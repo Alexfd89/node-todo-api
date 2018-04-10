681b5cee082899fef65e5f058f71b4f272063d32
Alex Fadeev <alexfd89@gmail.com>
2018-04-11 02:01:33 +0300

    Testing describe for DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,3 +100,43 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+
+    it('should remove a todo', (done) => {
+        var id = todos[0]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(id);
+        })
+        .end((err,res) => {
+            if(!err){
+                return done(err);
+            }
+
+            Todo.findById(id).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((e) => done(e));
+
+        });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        var id = new ObjectID();
+        request(app)
+        .delete(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+        .delete('/todos/123')
+        .expect(404)
+        .end(done);
+    });
+});
