ab02d01734d56bc89c6dd04b9fae45a9e73f713d
Alex Fadeev <alexfd89@gmail.com>
2018-04-11 03:42:42 +0300

    PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose'); 
 var {User} = require('./models/user');
 var {Todo} = require('./models/todo');
-var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -65,6 +66,29 @@ app.delete('/todos/:id', (req,res) => {
     }).catch((e) => {
         res.status(400).send();
     });
+});
+
+app.patch('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => res.status(400).send())
 });
 
 app.listen(port, () => {
