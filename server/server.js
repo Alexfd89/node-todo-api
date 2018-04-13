e9a3f8fe89a1ec16a6a310c11633845d7439a683
Alex Fadeev <alexfd89@gmail.com>
2018-04-13 21:16:29 +0300

    tokens

--- server/server.js
+++ server/server.js
@@ -14,6 +14,22 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+
+// POST /users
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+  
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x-auth', token).send(user);
+    }).catch((e) => {
+      res.status(400).send(e);
+    });
+  });
+  
+
 app.post('/todos', (req,res) => {
     var todo = new Todo({
         text: req.body.text
@@ -91,6 +107,8 @@ app.patch('/todos/:id', (req,res) => {
         res.send({todo});
     }).catch((e) => res.status(400).send())
 });
+
+
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
