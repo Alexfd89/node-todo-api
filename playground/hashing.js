1492b95bed0fdd2694575775c4b5142d66b8a9fd
Alex Fadeev <alexfd89@gmail.com>
2018-04-15 13:38:09 +0300

    Hash passwords using bcryptjs

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
+
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
-var token = jwt.sign(data,'123abc');
-console.log(token);
+var password = 'abc123!';
 
-var decoded = jwt.verify(token,'123abc');
-console.log('\n Decoded:', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash); 
+//     });
+// });
+
+var hashedPassword = '$2a$10$AOKXqCblf0tu.OUbOspQauFR5NCKI8sxs5EaQkWRMvQ6xdY3Cc2Yu';
+
+bcrypt.compare(password,hashedPassword, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 10
+// };
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token,'123abc');
+// console.log('\n Decoded:', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
