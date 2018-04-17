2d9886a7cd090d1101eb61dad4d190013d049cf1
Alex Fadeev <alexfd89@gmail.com>
2018-04-17 20:38:20 +0300

    Log out route

--- server/server.js
+++ server/server.js
@@ -112,6 +112,14 @@ app.post('/users', (req, res) => {
       res.send(req.user);
   });
 
+  app.delete('/users/me/token', authenticate, (req,res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, () => {
+        res.status(400).send();
+    });
+  });
+
   app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
 
