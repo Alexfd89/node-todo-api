6824ab3c7e2d2562c9cd20d884f492e1cd0906ff
Alex Fadeev <alexfd89@gmail.com>
2018-04-17 18:39:14 +0300

    Add POST /users/login

--- server/models/user.js
+++ server/models/user.js
@@ -70,6 +70,27 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user){
+      return Promise.reject();
+    }
+
+    return new Promise((reslove, reject) => {
+      bcrypt.compare(password, user.password, (err,res) => {
+        if(res){
+           reslove(user);
+        }else{
+          reject();
+        }
+      });
+    });
+  });
+};
+
+
 UserSchema.pre('save', function(next) {
   var user = this;
   if(user.isModified('password')){
--- server/server.js
+++ server/server.js
@@ -110,7 +110,20 @@ app.post('/users', (req, res) => {
 
   app.get('/users/me',authenticate, (req,res) => {
       res.send(req.user);
-  })
+  });
+
+  //POST /users/login {email, password}
+  app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send();
+    });
+  });
 
 
 
