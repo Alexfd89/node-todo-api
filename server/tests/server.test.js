915d8952d94d42976d24fe6f7700cfef75e65e4e
Alex Fadeev <alexfd89@gmail.com>
2018-04-17 21:21:02 +0300

    Add Test Logout

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -296,3 +296,25 @@ describe('POST /users/login', () => {
             .end(done);
     });
 });
+
+describe('DELETE /users/me/todo', () => {
+    it('should remove auth token on logout', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+});
