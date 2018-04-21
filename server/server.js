7a4da9d960a3c0efa602e21018ec9326847ad234
Alex Fadeev <alexfd89@gmail.com>
2018-04-21 12:52:03 +0300

    test

--- server/server.js
+++ server/server.js
@@ -110,6 +110,9 @@ app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
     user.save().then(() => {
       return user.generateAuthToken();
     }).then((token) => {
