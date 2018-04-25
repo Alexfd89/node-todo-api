bce16850c7861b3d166cdf6fa993ed5bdbe6aa55
Alex Fadeev <alexfd89@gmail.com>
2018-04-25 10:58:56 +0300

    test

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ if(env === "development" || env === "test"){
     });
 }
 
-
+///asd
 // if(env === 'development'){
 //     process.env.PORT = 3000;
 //     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
